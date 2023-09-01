ff93f41a189f17d029781ba2e37d68d56af22e41
ChemaSalazar <chemasalazar@outlook.com>
2023-08-31 22:09:10 -0500

    Refactoring: functions for automated processes.

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ REG2PARK_URL = "https://www.register2park.com/register"
               help='select file inside of profile dir')
 # @click.option('--test', prompt='Prevent submit?', default=True,
 #               help='This flag is created for testing before submitting the form to their server')
+
 def runProfile(profile):
     click.echo(f"your are loading profile: {profile} \n")
     file1 = open("profiles/" + profile + ".txt", "r")
@@ -36,65 +37,21 @@ def runProfile(profile):
         driver.get(REG2PARK_URL)
 
         # Auto enter property based on the profile you loaded, then submits to next page.
-        element = driver.find_element(By.ID, "propertyName")
-        element.send_keys(propName)
-        confirmBtn = driver.find_element(By.ID, "confirmProperty")
-        confirmBtn.click()
-
-        time.sleep(1)
+        populatePropertyForm(driver)
 
         # Auto select the property from list of options
-        radioOption = driver.find_element(By.CSS_SELECTOR, 'input[type="radio"][value="' + propRadioVal + '"]')
-        radioOption.click()
-
-        # Now click the submit the 'Next' button
-        confirmPropBtn = driver.find_element(By.ID, "confirmPropertySelection")
-        confirmPropBtn.click()
+        selectPropertyForm(driver)
 
         # Now click the Visitor Parking Button to continue
-        visitorParkingBtn = driver.find_element(By.ID, "registrationTypeVisitor")
-        visitorParkingBtn.click()
-
-        time.sleep(1)
+        clickVisitorParkingBtn(driver)
 
         # Now declare all form fields and send keys
-        apartmentNum_field = driver.find_element(By.ID, "vehicleApt")
-        apartmentNum_field.send_keys(aptNum)
-
-        vehicleMake_field = driver.find_element(By.ID, "vehicleMake")
-        vehicleMake_field.send_keys(vehMake)
-
-        vehicleModel_field = driver.find_element(By.ID, "vehicleModel")
-        vehicleModel_field.send_keys(vehModl)
-
-        licensePlate_field = driver.find_element(By.ID, "vehicleLicensePlate")
-        licensePlate_field.send_keys(vehPlts)
-
-        licensePlateCon_field = driver.find_element(By.ID, "vehicleLicensePlateConfirm")
-        licensePlateCon_field.send_keys(vehPlts)
+        fillDetailsForm(driver)
 
         # This section will not run by default, this is to prevent accidental submission when testing.
         # Reassign PREVENT_SUBMIT's value to False in order to submit the form.
         if not PREVENT_SUBMIT:
-            time.sleep(2)
-
-            submissionBtn = driver.find_element(By.ID, "vehicleInformation")
-            submissionBtn.click()
-
-            time.sleep(3)
-
-            emailConfirmModalBtn = driver.find_element(By.ID, "email-confirmation")
-            emailConfirmModalBtn.click()
-
-            time.sleep(2)
-
-            email_field = driver.find_element(By.ID, "emailConfirmationEmailView")
-            email_field.send_keys(profEmail)
-
-            emailSubmitBtn = driver.find_element(By.ID, "email-confirmation-send-view")
-            emailSubmitBtn.click()
-
-            time.sleep(4)
+            submitForm(driver)
 
 def setData(profile_data):
     global propName
@@ -144,5 +101,50 @@ def setUpWebDriver():
     # Prevent window from closing once instantiated.
     options.add_experimental_option("detach", True)
     return webdriver.Chrome(options=options)
+
+def populatePropertyForm(driver):
+    element = driver.find_element(By.ID, "propertyName")
+    element.send_keys(propName)
+    confirmBtn = driver.find_element(By.ID, "confirmProperty")
+    confirmBtn.click()
+    time.sleep(1)
+
+def selectPropertyForm(driver):
+    radioOption = driver.find_element(By.CSS_SELECTOR, 'input[type="radio"][value="' + propRadioVal + '"]')
+    radioOption.click()
+    # Now click the submit the 'Next' button
+    confirmPropBtn = driver.find_element(By.ID, "confirmPropertySelection")
+    confirmPropBtn.click()
+
+def clickVisitorParkingBtn(driver):
+    visitorParkingBtn = driver.find_element(By.ID, "registrationTypeVisitor")
+    visitorParkingBtn.click()
+    time.sleep(1)
+
+def fillDetailsForm(driver):
+    apartmentNum_field = driver.find_element(By.ID, "vehicleApt")
+    apartmentNum_field.send_keys(aptNum)
+    vehicleMake_field = driver.find_element(By.ID, "vehicleMake")
+    vehicleMake_field.send_keys(vehMake)
+    vehicleModel_field = driver.find_element(By.ID, "vehicleModel")
+    vehicleModel_field.send_keys(vehModl)
+    licensePlate_field = driver.find_element(By.ID, "vehicleLicensePlate")
+    licensePlate_field.send_keys(vehPlts)
+    licensePlateCon_field = driver.find_element(By.ID, "vehicleLicensePlateConfirm")
+    licensePlateCon_field.send_keys(vehPlts)
+
+def submitForm(driver):
+    time.sleep(2)
+    submissionBtn = driver.find_element(By.ID, "vehicleInformation")
+    submissionBtn.click()
+    time.sleep(3)
+    emailConfirmModalBtn = driver.find_element(By.ID, "email-confirmation")
+    emailConfirmModalBtn.click()
+    time.sleep(2)
+    email_field = driver.find_element(By.ID, "emailConfirmationEmailView")
+    email_field.send_keys(profEmail)
+    emailSubmitBtn = driver.find_element(By.ID, "email-confirmation-send-view")
+    emailSubmitBtn.click()
+    time.sleep(4)
 
 runProfile()
